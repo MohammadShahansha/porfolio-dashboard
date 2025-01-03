f022c8d82d2ce128da04eed8ceef0d036ef76d36
MohammadShahansha <shahansha.cou.ict11@gmail.com>
2025-01-04 02:02:42 +0600

    added text editor

--- src/app/(withDashboardLayout)/dashboard/blog/components/CreateBlogModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/blog/components/CreateBlogModal.tsx
@@ -8,7 +8,7 @@ import { toast } from "sonner";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { useState } from "react";
-
+// import CustomToolbar from './CustomToolbar'
 type TProps = {
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -16,7 +16,7 @@ type TProps = {
 
 const CreateBlogModal = ({ open, setOpen }: TProps) => {
   const [content, setContent] = useState("");
-
+  const [text, setText] = useState("");
   const [createBlog] = useCreateBlogMutation();
 
   const handleCreateBlog = async (values: FieldValues) => {
@@ -33,6 +33,45 @@ const CreateBlogModal = ({ open, setOpen }: TProps) => {
       toast.error("Somthing went wrong when created a Blog");
     }
   };
+
+  const modules = {
+    toolbar: {
+      container: [
+        [{ header: [1, 2, false] }],
+        ["bold", "italic", "underline", "strike", "blockquote"],
+        [
+          { list: "ordered" },
+          { list: "bullet" },
+          { indent: "-1" },
+          { indent: "+1" },
+        ],
+        ["link", "image"],
+        ["clean"],
+      ],
+    },
+  };
+  const formats = [
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "color",
+    "background",
+    "script",
+    "header",
+    "blockquote",
+    "code-block",
+    "indent",
+    "list",
+    "direction",
+    "align",
+    "link",
+    "image",
+    "video",
+    "formula",
+  ];
   return (
     <PSmallModal open={open} setOpen={setOpen} title="Add Your Skill">
       <PForm onSubmit={handleCreateBlog}>
@@ -45,8 +84,10 @@ const CreateBlogModal = ({ open, setOpen }: TProps) => {
             <ReactQuill
               value={content}
               onChange={(newContent) => setContent(newContent)}
+              modules={modules}
+              formats={formats}
             />
-            <input type="hidden" name="description" value={content} />
+            {/* <input type="hidden" name="description" value={content} /> */}
           </Grid>
         </Grid>
         <Box
